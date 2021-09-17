3f5ad821dd30fbf7f5b145fa30d4d7a71bab742a
DariyArtem <86294080+DariyArtem@users.noreply.github.com>
2021-09-17 19:38:24 +0300

    Update index.php

--- index.php
+++ index.php
@@ -8,22 +8,8 @@ spl_autoload_register(
         $filename = $root . $ds . str_replace('\\', $ds, $class) . '.php';
         require($filename);
     });
-
-
-// \Core\Router::get('/', 'HomeController@showPage');
-// \Core\Router::start();
 $db = new \App\Models\DB();
-//$result = $db::select("SELECT * FROM `users`");
-//var_dump( $result[0] );
-
-
 $a = new \Core\Router();
-//$a->add('', ['controller' => 'Main', 'action' => 'index']);
-//$a->add('registration', ['controller' => 'Registration', 'action' => 'index']);
-//$a->add('profile', ['controller' => 'Profile','action' => 'index']);
-//$a->add('login', ['controller' => 'Login', 'action' => 'index']);
-//$a->add('home', ['controller' => 'Home', 'action' => 'index']);
-//$a->add('other', ['controller' => 'OtherProfiles', 'action' => 'index']);
 $a->dispatch($_SERVER['REQUEST_URI']);
 
 
